55fd6a82b3838350c71659176b527e08e47693e3
purplewall1206 <wangzc5514@163.com>
2020-08-22 21:37:45 +0800

    cpio

--- null_pointer/boot.sh
+++ null_pointer/boot.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 qemu-system-x86_64 \
- -initrd rootfs.cpio \
+ -initrd rootfs1.cpio \
  -kernel bzImage \
  -nographic \
  -append "root=/dev/ram rw console=ttyS0 rdinit=/sbin/init quite oops=panic panic=1" \
  -m 128M \
- -monitor /dev/null \
+ -fsdev local,security_model=none,id=fsdev-fs0,path=/home/wangzc/Documents/k-hacks/null_pointer/share \
+ -device virtio-9p-pci,fsdev=fsdev-fs0,mount_tag=rootme  \
  -gdb tcp::1234 \
+ #-monitor /dev/null \
